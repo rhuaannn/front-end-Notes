e5d14859ea1f2b736881f76687d0b5c412709d06
rhuaannn <rhuanmarques1223@gmail.com>
2024-07-06 18:51:47 -0300

    style: components signIn center

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,9 +1,22 @@
-import {Container} from "./style";
+import { Container, Form } from "./style";
+import { Input } from "../../components/Input";
+import { Button } from "../../components/Button";
+import { FiMail, FiLock } from "react-icons/fi";
 
-export function SignIn(){
-    retrun(
+export function SignIn() {
+    return(
         <Container>
+            <Form>
+                <h1>Rocket NoTes!</h1>
+                <p>Seus links Salvos com sucecsso.</p>
+                <h2>Faça seu <strong>Login</strong></h2>
 
+                <Input placeholder="E-MAIL" type="text" icon={FiMail} />
+                <Input placeholder="Senha" type="password" icon={FiLock} />
+                <Button title='Entrar'/>
+
+                <a href="#">Criar conta</a>
+            </Form>
         </Container>
     );
 }
--- src/pages/SignIn/style.js
+++ src/pages/SignIn/style.js
@@ -1,7 +1,42 @@
-import styled from "styled-components"
+import styled from "styled-components";
 
 export const Container = styled.div`
     height: 100vh;
     display: flex;
-    align-items: stretch;
+    align-items: center;
+    justify-content:center;
 `;
+
+export const Form = styled.form`
+    padding: 0 136px;
+    display: flex;
+    flex-direction:column;
+    align-items:center;
+    text-align:center;
+    justify-content:center;
+
+    > h1{
+        font-size:48px;
+        color: ${({ theme }) => theme.COLORS.ORANGE}
+    }
+    >h2{
+        font-size: 24px;
+        margin-top:84px;
+        margin-bottom:24px;
+
+        >strong{
+            color: ${({ theme }) => theme.COLORS.ORANGE}
+        }
+    }
+    >p{
+        font-size:18px;
+        margin-top:15px;
+        text-align:center;
+        color: ${({ theme }) => theme.COLORS.GRAY_100};
+    }
+    >a{
+        margin-top:25px;
+        color: ${({ theme }) => theme.COLORS.ORANGE};
+    }
+    >
+    `;
